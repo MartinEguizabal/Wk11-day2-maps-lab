63060bec4389d58c82371f935ff9243ea634157f
MartyCodeClan <zabbalab@hotmail.com>
2017-09-05 17:00:36 +0100

    lab completed

---
+++ map_start_point/public/app.js
@@ -0,0 +1,18 @@
+var initialize = function() {
+  var center = {lat: 51.507351, lng: -0.127758};
+  var mapDiv = document.getElementById('main-map');
+  var mainMap = new MapWrapper(mapDiv, center, 10);
+
+  mainMap.addMarker(center);
+  mainMap.addClickEvent();
+
+  var bounceButton = document.querySelector('#button-bounce-markers');
+  bounceButton.addEventListener('click', mainMap.bounceMarkers.bind(mainMap));
+
+  window.addEventListener('resize', mainMap.resize);
+}
+
+window.addEventListener('load', initialize);
+
+// 55.993058
+// -3.289282
